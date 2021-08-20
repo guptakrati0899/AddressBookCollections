408fe27e78a9b58044e651c512285f998ae880fb
KratiGupta <kratigupta8545@gmail.com>
2021-08-20 12:45:02 +0530

    Added Welcome Message Successfully

---
+++ AddressBookMain.java
@@ -0,0 +1,8 @@
+package addressbook;
+
+public class AddressBookMain {
+        public static void main(String[] args) {
+            System.out.println("Welcome to the Address Book Problem");
+        }
+    }
+
